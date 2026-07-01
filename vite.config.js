776a2b5f89c93405cd7999cdf3952961341c9694
Prabhat Pathak <klaus007p@gmail.com>
2026-07-01 23:57:33 +0530

    last commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+// ensure `root` points to the project folder even if the CLI is invoked
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
+  root: __dirname,
+  base: './',
   plugins: [react()],
 })
